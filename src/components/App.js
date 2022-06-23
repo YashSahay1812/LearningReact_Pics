645351d6f9d821ffb264c18955d94f7ec55cfb0b
unknown <yashsahay999@gmail.com>
2022-06-23 23:20:44 +0530

    Successfully rendering images

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
 
-    // USING FETCH API : (Done by own) (CAN USE ASYNC AWAIT TOO)
-    // onSearchSubmit(term) {
-    //     console.log(term);
-    //     const params = new URLSearchParams({
-    //         client_id: '1absaLgm0YZorA-RmMlIedgjCXu70QO9wzyL7OqSTOU',
-    //         query: term
-    //     })
-    //     fetch('https://api.unsplash.com/search/photos/?' + params)
-    //         .then(res => res.json())
-    //         .then(res => {
-    //             console.log(res.results);
-    //         });
-    // }
-
     state = {images: []};
 
-    // USING AXIOS and ASYNC AWAIT:
-    // AXIOS also returns Promise just like fetch api
     onSearchSubmit = async (term) => {
         const response = await unsplash.get('/search/photos/',{
             params: {query: term}
@@ -36,7 +21,7 @@ class App extends React.Component {
         return (
             <div className='ui container' style={{ marginTop: '15px'}}>
                 <SearchBar onSubmit = {this.onSearchSubmit} />
-                Found: {this.state.images.length} images
+                <ImageList images = {this.state.images} />
             </div>
         );
         
